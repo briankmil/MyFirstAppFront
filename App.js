eee85944194b51431fdbaff5845447a02437b0b1
Camilo Pir <brianpiragauta@hotmail.com>
2024-03-11 17:57:28 -0500

    Add initial structure's project

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
+import React, { useState } from 'react'
+import { Button, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { MainNavigator } from './src/navigators/MainNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [personName, setPersonName] = useState('Camilo Pir');
+
+  const changeUserName = () => {
+    setPersonName("Brian Pir")
+    console.log(personName)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <MainNavigator />
+
+      </NavigationContainer>
+      <StatusBar
+        style='auto'
+        backgroundColor='pink'
+        translucent={true} />
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "5e5e5e",
+    // justifyContent: "center",
+    // alignItems: "center"
   },
+  // buttonStyle: {
+  //   borderRadius: 10,
+  //   // shadowColor: "black"
+  //   backgroundColor: "red",
+  //   padding: 10,
+  //   margin: 10,
+
+  // },
+  // textButtonStyle: {
+  //   color: "white"
+  // }
+
 });
